c8349028f31426fa7e8ad5f65828f1ee21e4d008
Jason Tsai <qrass06040130@gmail.com>
2023-10-02 10:42:34 +0800

    基礎-變數

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+
+
+$age=27;
+$name='Jason Tsai';
+echo $name;
+echo $age;
+
+$a=$age;
+$age=$name;
+$name=$a;
+
+
+    ?>
+</body>
+
+</html>
