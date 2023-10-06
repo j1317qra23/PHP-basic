fb93a71030f95e6befaf0171a2a3990e207fc353
Jason Tsai <qrass06040130@gmail.com>
2023-10-06 14:36:27 +0800

    add explain

--- 20231006/loop7.php
+++ 20231006/loop7.php
@@ -1,12 +1,17 @@
 <?php
-$a=['甲','乙','丙','丁','戊',];
+$a = ['甲', '乙', '丙', '丁', '戊', '己',];
 
-for ($i=0; $i < count($a); $i++) { 
-  echo $a[$i];
+// 使用for迴圈遍歷$a數組
+for ($i = 0; $i < count($a); $i++) {
+    // 輸出$a數組中的每個元素
+    echo $a[$i];
 }
 echo "<br>";
 
-foreach($a as $idx => $b) {
-  echo $idx ."=>" .$b;
-  echo "<br>";
+// 使用foreach迴圈遍歷$a數組，同時獲取索引和值
+foreach ($a as $idx => $b) {
+    // 輸出每個元素的索引和值
+    echo $idx . "=>" . $b;
+    echo "<br>";
 }
+?>
