f388c9954340e95ee51f1af0f9dfcaca38167e5d
Jason Tsai <qrass06040130@gmail.com>
2023-10-13 15:35:10 +0800

    矩形加對角線

---
+++ 20231013/20231013_09.php
@@ -0,0 +1,41 @@
+<style>
+    * {
+        font-family: 'Courier New', Courier, monospace;
+        line-height: 10px;
+    }
+</style>
+<h2>矩形</h2>
+<?php
+
+for($i=0;$i<7;$i++){
+    for($j=0;$j<7;$j++){
+        if($i==0 || $i==6){
+            echo "*"; 
+        } else if ($j==0 || $j==6){
+            echo "*"; 
+        }else {
+            echo "&nbsp;"; 
+        }
+     
+    }
+    echo "<br>";
+}
+?>
+
+<h2>矩形加對角線</h2>
+<?php
+
+for($i=0;$i<7;$i++){
+    for($j=0;$j<7;$j++){
+        if($i==0 || $i==6){
+            echo "*"; 
+        } else if ($j==0 || $j==6 || $j==$i || $i+$j==+6){
+            echo "*"; 
+        }else {
+            echo "&nbsp;"; 
+        }
+     
+    }
+    echo "<br>";
+}
+?>
