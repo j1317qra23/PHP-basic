aa65dc31b825627ed6260c7722afedb59bebdd43
Jason Tsai <qrass06040130@gmail.com>
2023-10-13 09:39:22 +0800

    九乘九

--- 20231013/20231013_01_9x9.php
+++ 20231013/20231013_01_9x9.php
@@ -1,15 +1,55 @@
+
 <h2>九九乘法表</h2>
 <?php
+
+<table border="1">;
+
 for($j=1;$j<=9;$j++){
-for ($i = 1; $i <= 9; $i++) { 
-    echo $j . ' * ' . $i . ' = ' . ($j * $i) . "&nbsp&nbsp&nbsp";
+    echo "<tr>";
+    for ($i=1;$i<=9;$i++){
+        echo "<td>";
+        echo $j . ' * '.$i.' = '.($j*$i). "<br>"echo "</td>";;
+    }
+    "</tr>"
+    // 迴圈走法 1*1 跑到1*9 跑完才換2*1
 }
-// 迴圈走法 1*1 跑到1*9 跑完才換2*1
+
+  
+</table>;
+/* "
 
 echo "<br>";
+for ($i=1;$i<=9;$i++){
+    echo '$j * '.$i.' = '.($j*$i). "&nbsp&nbsp&nbsp&nbsp";
 }
-
-
-
+echo "<br>";
+for ($i=1;$i<=9;$i++){
+    echo '$j * '.$i.' = '.($j*$i). "&nbsp&nbsp&nbsp&nbsp";
+}
+echo "<br>";
+for ($i=1;$i<=9;$i++){
+    echo '$j * '.$i.' = '.($j*$i). "&nbsp&nbsp&nbsp&nbsp";
+}
+echo "<br>";
+for ($i=1;$i<=9;$i++){
+    echo '$j * '.$i.' = '.($j*$i). "&nbsp&nbsp&nbsp&nbsp";
+}
+echo "<br>";
+for ($i=1;$i<=9;$i++){
+    echo '$j * '.$i.' = '.($j*$i). "&nbsp&nbsp&nbsp&nbsp";
+}
+echo "<br>";
+for ($i=1;$i<=9;$i++){
+    echo '$j * '.$i.' = '.($j*$i). "&nbsp&nbsp&nbsp&nbsp";
+}
+echo "<br>";
+for ($i=1;$i<=9;$i++){
+    echo '$j * '.$i.' = '.($j*$i). "&nbsp&nbsp&nbsp&nbsp";
+}
+echo "<br>";
+for ($i=1;$i<=9;$i++){
+    echo '$j * '.$i.' = '.($j*$i). "&nbsp&nbsp&nbsp&nbsp";
+}
+ */
 ?>
 
