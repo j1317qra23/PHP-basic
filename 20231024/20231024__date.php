b4f5a13227f64909ef94364aba751f769d97597f
Jason Tsai <qrass06040130@gmail.com>
2023-10-24 10:15:40 +0800

    計算生日天數

---
+++ 20231024/20231024__date.php
@@ -0,0 +1,33 @@
+<h1>日期與時間</h1>
+<?php
+date_default_timezone_set('asia/taipei');
+echo date("Y-m-d H:i:s");
+echo "<br>";
+echo date("n-j-l-W-N-l-Z-W-F-t-W-F-o-x-y-X-A-a-B-g-y-G");
+?>
+<h2> strtotime</h2>
+<?php
+echo strtotime("now");
+$time = strtotime("now");
+echo "<br>";
+echo date("Y-m-d H:i:s", $time);
+echo $date1 = '2023-10-24';
+echo $date2 = '2023-11-15';
+echo "<br>";
+$days = (strtotime($date2) - strtotime($date1)) / (60 * 60 * 24);
+echo $date1 . '到' . $date2 . "有" . $days . "天";
+?>
+<h2>計算下次生日天數</h2>
+<?php
+$date="1974-10-07";
+$br=strtotime($date);
+$diff=strtotime(date("Y")."-".date("m-d",$br));
+$today=strtotime('now');
+if($diff>$today){
+    $days=($diff-$today)/(60*60*24);
+}else{
+    $diff=strtotime("+1 year",$diff);
+    $days=($diff-$today)/(60*60*24);
+}
+
+echo "距離下一次生日:".date("Y-m-d",$diff)."還有".floor($days)."天";
