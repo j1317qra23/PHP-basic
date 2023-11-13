1b02b198007ee0a94cddbd12efb923045fb617a3
Jason Tsai <qrass06040130@gmail.com>
2023-11-13 16:13:52 +0800

    login member

--- 20231113_member/member.php
+++ 20231113_member/member.php
@@ -5,13 +5,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>首頁</title>
+    <title>會員中心</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 
-<body class="container">
-    <h1>哈哈購商城</h1>
+<body>
     <header class="nav">
         <div class="nav-item col-4"></div>
         <div class="nav-item col-4">
