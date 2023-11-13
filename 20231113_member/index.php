eabedfff9a2d1a8a4fafd80ec3bf840858a79f1f
Jason Tsai <qrass06040130@gmail.com>
2023-11-13 15:45:38 +0800

    login

--- 20231113_member/index.php
+++ 20231113_member/index.php
@@ -1,27 +1,38 @@
+<?php session_start();?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     <title>首頁</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
-
-<body>
-    <h1>購物商城</h1>
-    <header class="nav">
-        <div class="nav-item col-4"></div>
-        <div class="nav-item col-4">
-            <ul class='d-flex justify-content-evenly'>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-        </div class="nav-item" col-4 >
-        <a href="./reg.php" class="btn btn-primary mt-">註冊</a> <a href="login.php" class="btn btn-suscess">登入</a>
-    </header>
+<body class="container">
+<h1>哈哈購商城</h1>
+<header class="nav">
+    <div class="nav-item col-4"></div>
+    <div class="nav-item col-4">
+        <ul class='d-flex justify-content-evenly'>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </div>
+    <div class="nav-item col-4">
+        <?php
+        if(isset($_SESSION['user'])){
+            echo "歡迎光臨 ".$_SESSION['user'];
+            echo "<a href='logout.php' class='btn btn-info mx-2'>登出</a>";
+        }else{
+         ?>
+        <a href="reg.php" class="btn btn-primary mx-2">註冊</a>
+        <a href="login_form.php" class="btn btn-success mx-2">登入</a> 
+         <?php
+        }
+        ?>
+        
+    </div>
+</header>
 </body>
-
 </html>
