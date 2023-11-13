120db245825e2ec355d599ef583bf7c9f4089700
Jason Tsai <qrass06040130@gmail.com>
2023-11-13 15:14:46 +0800

    login

---
+++ 20231113_member/add_user.php
@@ -0,0 +1,12 @@
+
+<?php
+$dsn="mysql:host=localhost;charset=utf8;dbname=member";
+$pdo=new PDO($dsn,'root','');
+$acc=htmlspecialchars(trim($_POST['acc']));
+
+$sql="insert into `users`(`acc`,`pw`,`name`,`email`,`address`) 
+                   values('{$acc}','{$_POST['pw']}','{$_POST['name']}','{$_POST['email']}','{$_POST['address']}')";
+
+$pdo->exec($sql);
+
+header("Location:index.php");
---
+++ 20231113_member/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <title>首頁</title>
+</head>
+
+<body>
+    <h1>購物商城</h1>
+    <header class="nav">
+        <div class="nav-item col-4"></div>
+        <div class="nav-item col-4">
+            <ul class='d-flex justify-content-evenly'>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div class="nav-item" col-4 >
+        <a href="./reg.php" class="btn btn-primary mt-">註冊</a> <a href="login.php" class="btn btn-suscess">登入</a>
+    </header>
+</body>
+
+</html>
