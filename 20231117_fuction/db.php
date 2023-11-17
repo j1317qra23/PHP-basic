3897e12e2e75256739a827f5936ec089e11848f8
Jason Tsai <qrass06040130@gmail.com>
2023-11-17 14:20:22 +0800

    在函式中增加參數，提升泛用性

---
+++ 20231117_fuction/db.php
@@ -0,0 +1,22 @@
+<?php
+
+$rows=all('students');
+dd($rows);
+function all($table=null){
+    $dsn="mysql:host=localhost;charset=utf8;dbname=school";
+    $pdo=new PDO($dsn,'root','');
+    if(isset($table) && !empty($table)){
+     $sql="select * from `$table`";
+     $rows=$pdo->query($sql)->fetchall();
+     return $rows;
+    }else{
+        echo "錯誤:沒有指定的資料表名稱";
+     }
+}
+
+
+function dd($array){
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
