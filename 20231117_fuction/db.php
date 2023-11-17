f9dbda313bed5bd0b1c4e1f6087c0255ffe2f797
Jason Tsai <qrass06040130@gmail.com>
2023-11-17 14:34:08 +0800

    增加條件參數

--- 20231117_fuction/db.php
+++ 20231117_fuction/db.php
@@ -1,21 +1,23 @@
 <?php
 
-$rows=all('students');
+$rows = all('students',"where dept='1'");
 dd($rows);
-function all($table=null){
-    $dsn="mysql:host=localhost;charset=utf8;dbname=school";
-    $pdo=new PDO($dsn,'root','');
-    if(isset($table) && !empty($table)){
-     $sql="select * from `$table`";
-     $rows=$pdo->query($sql)->fetchall();
-     return $rows;
-    }else{
+function all($table = null,$where='')
+{
+    $dsn = "mysql:host=localhost;charset=utf8;dbname=school";
+    $pdo = new PDO($dsn, 'root', '');
+    if (isset($table) && !empty($table)) {
+        $sql = "select * from `$table` $where";
+        $rows = $pdo->query($sql)->fetchall();
+        return $rows;
+    } else {
         echo "錯誤:沒有指定的資料表名稱";
-     }
+    }
 }
 
 
-function dd($array){
+function dd($array)
+{
     echo "<pre>";
     print_r($array);
     echo "</pre>";
