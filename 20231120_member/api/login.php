ada21a24a14a51aa844368d09bb302219e9d88ee
Jason Tsai <qrass06040130@gmail.com>
2023-11-20 13:17:57 +0800

    member 調整檔案結構

---
+++ 20231120_member/api/login.php
@@ -0,0 +1,23 @@
+<?php
+include_once "./include/connect.php";
+$acc=$_POST['acc'];
+$pw=$_POST['pw'];
+
+
+//$sql="select * from users where `acc`='$acc' && `pw`='$pw'";
+$sql="select count(*) from users where `acc`='$acc' && `pw`='$pw'";
+
+//$user=$pdo->query($sql)->fetch();
+$user=$pdo->query($sql)->fetchColumn();
+//print_r($user);
+
+//if($user['acc']==$acc && $user['pw']==$pw){
+if($user){
+    $_SESSION['user']=$acc;
+    header("location:index.php");
+}else{
+    header('location:login_form.php?error=帳號密碼錯誤');
+}
+
+
+?>
