2f1094a4996006ea7de75bc06ce398e2dfbe504b
Jason Tsai <qrass06040130@gmail.com>
2023-11-24 11:05:05 +0800

    繼承 父類別 子類別

---
+++ 20231124_object/index3.php
@@ -0,0 +1,45 @@
+<?php
+
+class Animal{
+    protected $name;
+
+    public function __construct($name)
+    {
+        $this->name=$name;
+    }
+
+    public function setName($name){
+        $this->name=$name;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+
+}
+
+class Dog extends Animal{
+   function sit(){
+    echo $this->name;
+    echo "坐下";
+   }
+}
+$dog=new Dog('阿富');
+echo $dog->getname();
+echo "<br>";
+echo $dog->setName('阿旺');
+echo $dog->getname();
+
+$dog->sit();
+// $animal=new Animal('阿明'); //實例化 instant
+
+// echo '顯示名稱:'.$animal->getName();
+// echo "<br>";
+// $animal->setName('小花');
+// echo '顯示名稱:'.$animal->getName();
+// echo "<br>";
+// /* $animal->name='阿中';
+// echo '顯示名稱:'.$animal->name; */
+// echo "<br>";
+
+
