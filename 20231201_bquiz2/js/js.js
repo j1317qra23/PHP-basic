ceef07a233c95f4a7cae4970ae8833cc8b86b6fd
Jason Tsai <qrass06040130@gmail.com>
2023-12-01 08:59:50 +0800

    完成更新圖片功能

--- 20231201_bquiz2/js/js.js
+++ 20231201_bquiz2/js/js.js
@@ -20,13 +20,10 @@ function lo(x)
 function op(x,y,url)
 {
 	$(x).fadeIn()
-	if(y){
-		$(y).fadeIn()
-	}
-	
-	if(y&&url){
-		$(y).load(url)
-	}
+	if(y)
+	$(y).fadeIn()
+	if(y&&url)
+	$(y).load(url)
 }
 function cl(x)
 {
