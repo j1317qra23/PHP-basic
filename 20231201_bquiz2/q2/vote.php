8d58f39ef683a1d1e5aaa914fede867bf418c051
Jason Tsai <qrass06040130@gmail.com>
2023-12-01 13:58:26 +0800

    建立前台投票列表頁面

---
+++ 20231201_bquiz2/q2/vote.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>問卷投票</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+</head>
+<body>
+<header class="p-5">
+    <h1 class="text-center">問卷投票</h1>
+</header>   
+<main>
+    <?
+</main>
+<script src="../js/jquery-3.4.1.min.js"></script>
+<script src="../js/bootstrap.js"></script> 
+</body>
+
+</html>
