a2d2ad16ead75e78e87f08c19a37b97ff306fa47
Jason Tsai <qrass06040130@gmail.com>
2023-12-08 10:01:15 +0800

    將進站總人數讀出並顯示在網站上  & 複製total.php 為bottom.php並修改相關的變數及資料表,完成頁尾版權功能

--- 20231208_web91/back.php
+++ 20231208_web91/back.php
@@ -16,8 +16,7 @@
         <div id="cvr" style="position:absolute; width:99%; height:100%; margin:auto; z-index:9898;"></div>
     </div>
 </div>
-記得刪除
-<!-- <iframe style="display:none;" name="back" id="back"></iframe> -->
+
 	<div id="main">
     	<a title="" href="?"><div class="ti" style="background:url(&#39;use/&#39;); background-size:cover;"></div><!--標題--></a>
         	<div id="ms">
@@ -65,8 +64,7 @@
 							                            
 							                    </div>
                     <div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
-                    	<span class="t">進站總人數 : 
-                        	1                        </span>
+                    	<span class="t">進站總人數 :<?=$Total->find(1)['total'];?></span>
                     </div>
         		</div>
                 <div class="di" style="height:540px; border:#999 1px solid; width:76.5%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
@@ -107,7 +105,7 @@
                              </div>
              	<div style="clear:both;"></div>
             	<div style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
-                	<span class="t" style="line-height:123px;"></span>
+                	<span class="t" style="line-height:123px;"><?=$Bottom->find(1)['bottom'];?></span>
                 </div>
     </div>
 
