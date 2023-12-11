6e6dbcfd8f3bac3e19d54cc244049868e0c1e802
Jason Tsai <qrass06040130@gmail.com>
2023-12-11 09:24:22 +0800

    複製/back/title.php 為 mvim.php 並修改頁面文字及建立資料表

--- 20231208_web91/modal/upload.php
+++ 20231208_web91/modal/upload.php
@@ -1,45 +1,44 @@
-<?php
-switch ($_GET['table']) {
-    case 'title':
+<?php 
+switch($_GET['table']){
+    case "title":
         echo "<h3>更新網站標題圖片</h3>";
-        break;
+    break;
     case 'mvim':
         echo "<h3>更換動畫圖片</h3>";
-        break;
+    break;
     case 'image':
-        echo "<h3>校園動畫圖片</h3>";
-        break;
+        echo "<h3>更新校園映像圖片</h3>";
+    break;
+
 }
 ?>
 
 <hr>
 <form action="./api/update.php" method="post" enctype="multipart/form-data">
-    <table>
-        <tr>
-            <?php
-       
-            switch ($_GET['table']) {
-                case 'title':
-                    echo "<h3>標題區圖片</h3>";
-                    break;
-                case 'mvim':
-                    echo "<h3>動畫圖片</h3>";
-                    break;
-                case 'image':
-                    echo "<h3>校園映像圖片</h3>";
-                    break;
-            }
-            ?>
-          
-            <td><input type="file" name="img" id=""></td>
-        </tr>
-
-    </table>
-    <div>
-        <input type="hidden" name="table" value="<?= $_GET['table']; ?>">
-        <input type="hidden" name="id" value="<?= $_GET['id']; ?>">
-        <input type="submit" value="更新">
-        <input type="reset" value="重置">
-    </div>
+<table>
+    <tr>
+    <?php 
+        switch($_GET['table']){
+            case "title":
+                echo "<td>標題區圖片</td>";
+            break;
+            case 'mvim':
+                echo "<td>動畫圖片</td>";
+            break;
+            case 'image':
+                echo "<td>校園映像圖片</td>";
+            break;
+        }
+    ?>
+        
+        <td><input type="file" name="img" id=""></td>
+    </tr>
+</table>
+<div>
+    <input type="hidden" name="table" value="<?=$_GET['table'];?>">
+    <input type="hidden" name="id" value="<?=$_GET['id'];?>">
+    <input type="submit" value="更新">
+    <input type="reset" value="重置">
+</div>
 
 </form>
--- 20231211_web91/modal/mvim.php
+++ 20231211_web91/modal/mvim.php
@@ -3,12 +3,8 @@
 <form action="./api/add.php" method="post" enctype="multipart/form-data">
 <table>
     <tr>
-        <td>動畫圖片</td>
+        <td>動畫圖片:</td>
         <td><input type="file" name="img" id=""></td>
-    </tr>
-    <tr>
-        <td>標題區替代文字</td>
-        <td><input type="text" name="text" id=""></td>
     </tr>
 </table>
 <div>
