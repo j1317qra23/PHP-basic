a97bd859fa63e2c0134b8c0806703d8d331baf2e
Jason Tsai <qrass06040130@gmail.com>
2023-12-11 15:06:33 +0800

    重整edit.php及各功能的資料陣列做法,提高api的相容性

--- 20231211_dif_web91/back/ad.php
+++ 20231211_dif_web91/back/ad.php
@@ -1,31 +1,28 @@
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
-    <p class="t cent botli">動畫圖片管理</p>
+    <p class="t cent botli">動態文字廣告管理</p>
     <form method="post" action="./api/edit.php">
         <table width="100%" style="text-align: center">
             <tbody>
                 <tr class="yel">
-                    <td width="70%">動畫圖片</td>
+                    <td width="80%">動態文字廣告</td>
                     <td width="10%">顯示</td>
                     <td width="10%">刪除</td>
-                    <td></td>
                 </tr>
                 <?php
-                $rows=$Mvim->all();
+
+                $rows=$DB->all();
                 foreach($rows as $row){
                 ?>
                 <tr>
                     <td>
-                        <img src="./img/<?=$row['img'];?>" style="width:150px;height:100px">
+                        <input type="text" name="text[<?=$row['id'];?>]" style="width:90%" value="<?=$row['text'];?>">
+                        <input type="hidden" name="id[]" value="<?=$row['id'];?>">
                     </td>
-                    <input type="hidden" name="id[]" value="<?=$row['id'];?>">
                     <td>
                         <input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked':'';?>>
                     </td>
                     <td>
                     <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
-                    </td>
-                    <td>
-                    <input type="button" onclick="op('#cover','#cvr','./modal/upload.php?table=<?=$do;?>&id=<?=$row['id'];?>')" value="更換動畫">
                     </td>
                 </tr>
                 <?php
@@ -37,7 +34,7 @@
             <tbody>
                 <tr>
                     <input type="hidden" name="table" value="<?=$do;?>">
-                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?=$do;?>.php?table=<?=$do;?>')" value="新增動畫圖片"></td>
+                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?=$do;?>.php?table=<?=$do;?>')" value="新增動態文字廣告"></td>
                     <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
                 </tr>
             </tbody>
--- 20231211_dif_web91/back/mvim.php
+++ 20231211_dif_web91/back/mvim.php
@@ -1,33 +1,33 @@
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
-    <p class="t cent botli">動態文字廣告管理</p>
+    <p class="t cent botli">動畫圖片管理</p>
     <form method="post" action="./api/edit.php">
         <table width="100%" style="text-align: center">
             <tbody>
                 <tr class="yel">
-                 
-                    <td width="80%">動態文字廣告</td>
+                    <td width="70%">動畫圖片</td>
                     <td width="10%">顯示</td>
                     <td width="10%">刪除</td>
-              
+                    <td></td>
                 </tr>
                 <?php
-                $rows=$Ad->all();
+
+                $rows=$DB->all();
                 foreach($rows as $row){
                 ?>
                 <tr>
-                    
-                    <td width=>
-                        <input type="text" name="text[<?=$row['id'];?>]" style="width:90%" value="<?=$row['text'];?>">
-                        
+                    <td>
+                        <img src="./img/<?=$row['img'];?>" style="width:150px;height:100px">
                     </td>
-                    <td width=>
-                    <input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked':'';?>>
+                    <input type="hidden" name="id[]" value="<?=$row['id'];?>">
+                    <td>
+                        <input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked':'';?>>
                     </td>
-                    <td width=>
+                    <td>
                     <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
                     </td>
                     <td>
-                  
+                    <input type="button" onclick="op('#cover','#cvr','./modal/upload.php?table=<?=$do;?>&id=<?=$row['id'];?>')" value="更換動畫">
+                    </td>
                 </tr>
                 <?php
                 }
@@ -38,7 +38,7 @@
             <tbody>
                 <tr>
                     <input type="hidden" name="table" value="<?=$do;?>">
-                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?=$do;?>.php?table=<?=$do;?>')" value="新增動態文字廣告圖片"></td>
+                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?=$do;?>.php?table=<?=$do;?>')" value="新增動畫圖片"></td>
                     <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
                 </tr>
             </tbody>
