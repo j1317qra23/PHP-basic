4cfe8030d9618ce6b238982bfa7b87afe14dca07
Jason Tsai <qrass06040130@gmail.com>
2023-12-11 09:32:13 +0800

    還原

--- 20231208_web91/modal/upload.php
+++ 20231208_web91/modal/upload.php
@@ -1,43 +1,16 @@
-<?php 
-switch($_GET['table']){
-    case "title":
-        echo "<h3>更新網站標題圖片</h3>";
-    break;
-    case 'mvim':
-        echo "<h3>更換動畫圖片</h3>";
-    break;
-    case 'image':
-        echo "<h3>更新校園映像圖片</h3>";
-    break;
-
-}
-?>
-
+<h3>更新網站標題圖片</h3>
 <hr>
 <form action="./api/update.php" method="post" enctype="multipart/form-data">
 <table>
     <tr>
-    <?php 
-        switch($_GET['table']){
-            case "title":
-                echo "<td>標題區圖片</td>";
-            break;
-            case 'mvim':
-                echo "<td>動畫圖片</td>";
-            break;
-            case 'image':
-                echo "<td>校園映像圖片</td>";
-            break;
-        }
-    ?>
-        
+        <td>標題區圖片</td>
         <td><input type="file" name="img" id=""></td>
     </tr>
 </table>
 <div>
     <input type="hidden" name="table" value="<?=$_GET['table'];?>">
-    <input type="hidden" name="id" value="<?=$_GET['id'];?>">
-    <input type="submit" value="更新">
+    <input type="hidden" name="table" value="<?=$_GET['id'];?>">
+    <input type="submit" value="新增">
     <input type="reset" value="重置">
 </div>
 
