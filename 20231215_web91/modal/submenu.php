c1c58c19d18b70d8dfe74d53c61df7509cf84917
Jason Tsai <qrass06040130@gmail.com>
2023-12-15 10:14:18 +0800

    完成編輯次選單前端畫面及表單設定,設定submenu api程式概況

--- 20231215_web91/modal/submenu.php
+++ 20231215_web91/modal/submenu.php
@@ -2,7 +2,7 @@
 <h3>編輯次選單</h3>
 <hr>
 <form action="./api/submenu.php" method="post" enctype="multipart/form-data">
-<table class='cent'>
+<table class='cent' id="sub">
     <tr>
         <td>次選單名稱</td>
         <td>次選單連結網址</td>
@@ -30,7 +30,7 @@
     <input type="hidden" name="table" value="<?=$_GET['table'];?>">
     <input type="submit" value="修改確定">
     <input type="reset" value="重置">
-    <input type="button" value="更多次選單">
+    <input type="button" value="更多次選單" onclick="more()">
 </div>
 
 </form>
@@ -42,7 +42,7 @@ function more(){
                 <td><input type="text" name="href[]" id=""></td>
               </tr>`
 
-    $("#xxx").append(item);
+    $("#sub").append(item);
 
 }
 
