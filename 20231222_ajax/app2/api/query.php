41770f26fb2616c8c22813713bfa0a5231c01373
Jason Tsai <qrass06040130@gmail.com>
2023-12-22 15:46:26 +0800

    使用ajax來載入全部學生資料

--- 20231222_ajax/app2/api/query.php
+++ 20231222_ajax/app2/api/query.php
@@ -1,5 +1,21 @@
 <?php
+include_once "db.php";
 //處理查詢資料的請求
-
+switch($_GET['do']){
+    case "all":
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode($Student->all());
+    break;
+    case 'sex':
+        $users=$Student->q("select `name`,`uni_id`,`school_num`,`birthday` from `students` where substr(`uni_id`,2,1)='{$_GET['value']}'");
+        
+        header('Content-Type: application/json; charset=utf-8');        
+        echo json_encode($users);
+    break;
+    case 'class':
+            
+            dd($_GET['value']);
+    break;
+}
 
 ?>
