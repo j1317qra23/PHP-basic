71dad8db3def19b420d18318a6e0e4b957ab9c6e
Jason Tsai <qrass06040130@gmail.com>
2023-12-29 15:33:14 +0800

    在登入程式中加上session,並根據登入狀況來顯示不同的內容

--- 20231229_web15/index.php
+++ 20231229_web15/index.php
@@ -18,7 +18,9 @@
 	</div>
 	<div id="all">
 		<div id="title">
-			<?= date("m月d日 l"); ?> | 今日瀏覽: <?=$Total->find(['date'=>date('Y-m-d')])['total'];?> | 累積瀏覽: <?=$Total->sum('total');?>
+			<?= date("m月d日 l"); ?> | 
+			今日瀏覽: <?=$Total->find(['date'=>date("Y-m-d")])['total'];?> | 
+			累積瀏覽: <?=$Total->sum('total');?>
 			<a href="index.php" style='float:right'>回首頁</a>
 		</div>
 		<div id="title2" title='健康促進網-回首頁'>
@@ -37,17 +39,29 @@
 					<marquee style="width:78%; display:inline-block;">請民眾踴躍投稿電子報，讓電子報成為大家相互交流、分享的園地！詳見最新文章</marquee>
 
 					<span style="width:20%; display:inline-block;">
+					<?php 
+					if(!isset($_SESSION['user'])){
+					?>
 						<a href="?do=login">會員登入</a>
+					<?php 
+					}else{
+					?>		
+						歡迎,<?=$_SESSION['user'];?> 
+						<button>登出</button>
+					<?php 
+					}
+					?>						
 					</span>
 					<div class="">
 						<?php
-						$do=$_GET['do']??'main';
-						$file="./front/{$do}.php";
-						if(file_exists($file)){
-							include $file;
-						}else{
-							include "./front/main.php";
-						}
+							$do=$_GET['do']??'main';
+							$file="./front/{$do}.php";
+							if(file_exists($file)){
+								include $file;
+							}else{
+								include "./front/main.php";
+							}
+
 						?>
 					</div>
 				</div>
@@ -56,7 +70,7 @@
 		<div id="bottom">
 			本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2012健康促進網社群平台 All Right Reserved
 			<br>
-			服務信箱：health@test.labor.gov.tw<img src="./icon/02B02.jpg" width="45">
+			服務信箱：health@test.labor.gov.tw<img src="./home_files/02B02.jpg" width="45">
 		</div>
 	</div>
 
