dd916a1a723961530c6faf1127dfe02afac5141f
Jason Tsai <qrass06040130@gmail.com>
2024-01-05 08:47:49 +0800

    0105

---
+++ 20240105_web15/front/main.php
@@ -0,0 +1,13 @@
+<style>
+    .tag{
+        width: 120px;
+        padding: 5px;
+        border: 1px solid black;
+    }
+</style>
+<div class="tags">
+    <class class="tag"></class>
+    <class class="tag"></class>
+    <class class="tag"></class>
+    <class class="tag"></class>
+</div>
