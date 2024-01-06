3ba508e8d6428353e46be7c903611b691ccf77ef
Jason Tsai <qrass06040130@gmail.com>
2024-01-07 00:13:28 +0800

    homework

--- 20231219_web_homework/index.php
+++ 20231219_web_homework/index.php
@@ -17,14 +17,14 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
 
   <!-- Bootstrap CSS File -->
-  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="lib01/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Libraries CSS Files -->
-  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-  <link href="lib/animate/animate.min.css" rel="stylesheet">
-  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
-  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-  <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+  <link href="lib01/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib01/animate/animate.min.css" rel="stylesheet">
+  <link href="lib01/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib01/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib01/lightbox/css/lightbox.min.css" rel="stylesheet">
 
   <!-- Main Stylesheet File -->
   <link href="css/style.css" rel="stylesheet">
@@ -662,19 +662,19 @@
   <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
 
   <!-- JavaScript Libraries -->
-  <script src="lib/jquery/jquery.min.js"></script>
-  <script src="lib/jquery/jquery-migrate.min.js"></script>
-  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="lib/easing/easing.min.js"></script>
-  <script src="lib/superfish/hoverIntent.js"></script>
-  <script src="lib/superfish/superfish.min.js"></script>
-  <script src="lib/wow/wow.min.js"></script>
-  <script src="lib/waypoints/waypoints.min.js"></script>
-  <script src="lib/counterup/counterup.min.js"></script>
-  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
-  <script src="lib/isotope/isotope.pkgd.min.js"></script>
-  <script src="lib/lightbox/js/lightbox.min.js"></script>
-  <script src="lib/touchSwipe/jquery.touchSwipe.min.js"></script>
+  <script src="lib01/jquery/jquery.min.js"></script>
+  <script src="lib01/jquery/jquery-migrate.min.js"></script>
+  <script src="lib01/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib01/easing/easing.min.js"></script>
+  <script src="lib01/superfish/hoverIntent.js"></script>
+  <script src="lib01/superfish/superfish.min.js"></script>
+  <script src="lib01/wow/wow.min.js"></script>
+  <script src="lib01/waypoints/waypoints.min.js"></script>
+  <script src="lib01/counterup/counterup.min.js"></script>
+  <script src="lib01/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib01/isotope/isotope.pkgd.min.js"></script>
+  <script src="lib01/lightbox/js/lightbox.min.js"></script>
+  <script src="lib01/touchSwipe/jquery.touchSwipe.min.js"></script>
   <!-- Contact Form JavaScript File -->
   <script src="contactform/contactform.js"></script>
 
