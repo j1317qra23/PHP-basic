8ad607a23480cd86d6ad33342db7bc0c97d8063a
Jason Tsai <qrass06040130@gmail.com>
2024-01-08 11:18:36 +0800

    homework 註冊系統

--- 20231219_web_homework/front/login.php
+++ 20231219_web_homework/front/login.php
@@ -82,7 +82,31 @@ if(isset($_GET['error'])){
             <p class="cent">帳號：<input name="acc" autofocus="" type="text"></p>
             <p class="cent">密碼：<input name="pw" type="password"></p>
             <p class="cent"><input value="送出" type="submit"><input type="reset" value="清除"></p>
+            <p class="cent"><a href="?do=forget">忘記密碼</a> | <a href="?do=reg">尚未註冊</a> </p>
         </form>
     </div>
+    <script>
+function login(){
+    $.post('./api/chk_acc.php',{acc:$("#acc").val()},(res)=>{
+        if(parseInt(res)==0){
+            alert("查無帳號")
+        }else{
+            $.post('./api/chk_pw.php',
+                   {acc:$("#acc").val(),pw:$("#pw").val()},
+                   (res)=>{
+                        if(parseInt(res)==1){
+                            if($("#acc").val()=='admin'){
+                                location.href='back.php'
+                            }else{
+                                location.href='index.php'
+                            }
+                        }else{
+                            alert("密碼錯誤")
+                        }
+            })
+        }
+    })
+}
+</script>
 </body>
 </html>
