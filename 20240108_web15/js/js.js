05038be80967cd253dedefa248ed08718f4147c8
Jason Tsai <qrass06040130@gmail.com>
2024-01-08 14:05:16 +0800

    解釋原程式ajax的使用方法

--- 20240108_web15/js/js.js
+++ 20240108_web15/js/js.js
@@ -6,21 +6,15 @@ function lo(th,url)
 function good(news){
 	$.post("./api/good.php",{news},()=>{
 		location.reload();
+/* 		switch($("#n"+news).text()){
+			case "讚":
+				$("#n"+news).text("收回讚")
+				$("#g"+news).text($("#g"+news).text()*1+1)
+			break;
+			case "收回讚":
+				$("#n"+news).text("讚")
+				$("#g"+news).text($("#g"+news).text()*1-1)
+			break;
+		} */
 	})
-	}
-// function good(id,type,user)
-// {
-// 	$.post("back.php?do=good&type="+type,{"id":id,"user":user},function()
-// 	{
-// 		if(type=="1")
-// 		{
-// 			$("#vie"+id).text($("#vie"+id).text()*1+1)
-// 			$("#good"+id).text("收回讚").attr("onclick","good('"+id+"','2','"+user+"')")
-// 		}
-// 		else
-// 		{
-// 			$("#vie"+id).text($("#vie"+id).text()*1-1)
-// 			$("#good"+id).text("讚").attr("onclick","good('"+id+"','1','"+user+"')")
-// 		}
-// 	})
-// }
+}
