b63cfc132285fb5d64f8ebdef6f54f6d264fd7dd
Jason Tsai <qrass06040130@gmail.com>
2024-01-08 13:58:18 +0800

    使用ajax完成按讚功能

--- 20240108_web15/front/news.php
+++ 20240108_web15/front/news.php
@@ -34,13 +34,11 @@
             <?php
             if(isset($_SESSION['user'])){
                 if($Log->count(['news'=>$row['id'],'acc'=>$_SESSION['user']])>0){
-                    echo "<a href=''>收回讚</a>";
+                    echo "<a href='Javascript:good({$row['id']})'>收回讚</a>";
                 }else{
-                    echo "<a href=''>讚</a>";
+                    echo "<a href='Javascript:good({$row['id']})'>讚</a>";
                 }
             }
-            
-
             ?>
             </td>
         </tr>
@@ -73,5 +71,5 @@ $(".title").on('click',(e)=>{
 
 })
 
-
 </script>
+
