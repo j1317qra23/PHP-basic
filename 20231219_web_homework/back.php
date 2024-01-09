ec0b7e0c165580f4866abcdb2f586e7fbc452e34
Jason Tsai <qrass06040130@gmail.com>
2024-01-09 19:20:57 +0800

    homework

--- 20231219_web_homework/back.php
+++ 20231219_web_homework/back.php
@@ -28,7 +28,12 @@ if(!isset($_SESSION['login'])){
 </head>
 
 <body id="page-top">
-
+<div id="cover" style="display:none; ">
+		<div id="coverr">
+			<a style="position:absolute; right:3px; top:4px; cursor:pointer; z-index:9999;" onclick="cl(&#39;#cover&#39;)">X</a>
+			<div id="cvr" style="position:absolute; width:99%; height:100%; margin:auto; z-index:9898;"></div>
+		</div>
+	</div>
     <!-- Page Wrapper -->
     <div id="wrapper">
 
