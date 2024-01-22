de5006b2ad6883773748939367fcd5aef44a615e
Jason Tsai <qrass06040130@gmail.com>
2024-01-22 16:08:17 +0800

    修正表單password欄位設定

--- 20240122_web03/front/admin.php
+++ 20240122_web03/front/admin.php
@@ -22,3 +22,24 @@
     </tr>
 </table>
 <div class="ct"><button>確認</button></div>
+<script>
+function login(table){
+    $.get('./api/chk_ans.php',{ans:$("#ans").val()},(chk)=>{
+        if(parseInt(chk)==0){
+            alert("對不起，您輸入的驗證碼有誤，請您重新登入")
+        }else{
+            $.post("./api/chk_pw.php",
+                    {table,
+                     acc:$("#acc").val(),
+                     pw:$("#pw").val()},
+                    (res)=>{
+                if(parseInt(res)==0){
+                    alert("帳號或密碼錯誤，請重新輸入")
+                }else{
+                    location.href='back.php?do=admin';
+                }
+            })
+        }
+    })
+}
+</script>
--- 20240122_web03/front/login.php
+++ 20240122_web03/front/login.php
@@ -10,7 +10,7 @@
     </tr>
     <tr>
         <td class="tt">密碼</td>
-        <td class="pp"><input type="text" name="pw" id="pw"></td>
+        <td class="pp"><input type="password" name="pw" id="pw"></td>
     </tr>
     <tr>
         <td class="tt">驗證碼</td>
@@ -29,18 +29,18 @@
 </div>
 <script>
 function login(table){
-    $.get('./api/chk_ans.php',{ans:$("#ans").val()},(chk)=>
-    {
-        if(parseInt(chk)==0)
-        {
+    $.get('./api/chk_ans.php',{ans:$("#ans").val()},(chk)=>{
+        if(parseInt(chk)==0){
             alert("驗證碼錯誤，請重新輸入")
         }else{
-            $.post("./api/chk_pw.php",{table,acc:$("#acc").val(),pw:$("#pw").val()},(res)=>{
-                if(parseInt(res)==0)
-                {
+            $.post("./api/chk_pw.php",
+                    {table,
+                     acc:$("#acc").val(),
+                     pw:$("#pw").val()},
+                    (res)=>{
+                if(parseInt(res)==0){
                     alert("帳號或密碼錯誤，請重新輸入")
-                }else
-                {
+                }else{
                     location.href='index.php';
                 }
             })
