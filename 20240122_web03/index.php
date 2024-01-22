80abd9f6828c646790bf76fd7f2ba980c2855c98
Jason Tsai <qrass06040130@gmail.com>
2024-01-22 13:49:57 +0800

    建立登入頁面版型

--- 20240122_web03/index.php
+++ 20240122_web03/index.php
@@ -27,7 +27,7 @@
                 <a href="?do=admin">管理登入</a>
             </div>
             <marquee>
-                年終特賣會開跑了 &nbsp; 情人節特惠活動 &nbsp;
+                年終特賣會開跑了 &nbsp;  情人節特惠活動 &nbsp; 
             </marquee>
         </div>
         <div id="left" class="ct">
@@ -40,18 +40,19 @@
             </span>
         </div>
         <div id="right">
-            <?php
-            $do = $_GET['do'] ?? 'main';
-            $file = "./front/{$do}.php";
-            if (file_exists($file)) {
-                include $file;
-            } else {
-                include "./front/main.php";
-            }
+            
+            <?php 
+                $do=$_GET['do']??'main';
+                $file="./front/{$do}.php";
+                if(file_exists($file)){
+                    include $file;
+                }else{
+                    include "./front/main.php";
+                }
             ?>
         </div>
         <div id="bottom" style="line-height:70px;background:url(icon/bot.png); color:#FFF;" class="ct">
-            <?= $Bottom->find(1)['bottom']; ?></div>
+           <?=$Bottom->find(1)['bottom'];?></div>
     </div>
     <script src="./js/jquery-3.4.1.min.js"></script>
 </body>
