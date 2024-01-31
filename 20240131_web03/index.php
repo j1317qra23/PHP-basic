049c27844c275f22c2a54f529a7792e406cc8f92
Jason Tsai <qrass06040130@gmail.com>
2024-01-31 08:40:43 +0800

    增加登出功能

--- 20240131_web03/index.php
+++ 20240131_web03/index.php
@@ -24,23 +24,30 @@
                 <a href="?do=look">購物流程</a> |
                 <a href="?do=buycart">購物車</a> |
                 <?php
-                if (isset($_SESSION['mem'])) { ?>
-                    <a href="?do=login">登出</a> |
+                if(isset($_SESSION['mem'])){
+                ?>
+                    <a href="./api/logout.php">登出</a> |
                 <?php
-                } else { ?>
+                }else{
+                ?>
                     <a href="?do=login">會員登入</a> |
-                <?php } ?>
-
                 <?php
-                if (isset($_SESSION['admin'])) { ?>
-                    <a href="?do=admin">返回管理</a>
+                }
+                ?>
                 <?php
-                } else { ?>
+                if(isset($_SESSION['admin'])){
+                ?>
+                    <a href="back.php">返回管理</a>
+                <?php 
+                }else{
+                ?>
                     <a href="?do=admin">管理登入</a>
-                <?php } ?>
+                <?php    
+                }
+                ?>
             </div>
             <marquee>
-                年終特賣會開跑了 &nbsp; 情人節特惠活動 &nbsp;
+                年終特賣會開跑了 &nbsp;  情人節特惠活動 &nbsp; 
             </marquee>
         </div>
         <div id="left" class="ct">
@@ -53,19 +60,19 @@
             </span>
         </div>
         <div id="right">
-
-            <?php
-            $do = $_GET['do'] ?? 'main';
-            $file = "./front/{$do}.php";
-            if (file_exists($file)) {
-                include $file;
-            } else {
-                include "./front/main.php";
-            }
+            
+            <?php 
+                $do=$_GET['do']??'main';
+                $file="./front/{$do}.php";
+                if(file_exists($file)){
+                    include $file;
+                }else{
+                    include "./front/main.php";
+                }
             ?>
         </div>
         <div id="bottom" style="line-height:70px;background:url(icon/bot.png); color:#FFF;" class="ct">
-            <?= $Bottom->find(1)['bottom']; ?></div>
+           <?=$Bottom->find(1)['bottom'];?></div>
     </div>
     <script src="./js/jquery-3.4.1.min.js"></script>
 </body>
