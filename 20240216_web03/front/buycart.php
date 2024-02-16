1b37f819b14eee79e5cb71e8a3d2f51b81c8a5b2
Jason Tsai <qrass06040130@gmail.com>
2024-02-16 14:50:26 +0800

    建立使用session來紀錄購物車的機制

--- 20240216_web03/front/buycart.php
+++ 20240216_web03/front/buycart.php
@@ -3,3 +3,20 @@ if(!isset($_SESSION['mem'])){
 to("?do=login");
 }
 
+echo "<h2 class='ct'>{$_SESSION['mem']}的購物車</h2>";
+
+if(!isset($_SESSION['cart'])){
+    echo "<h2 class='ct'>購物車中尚無商品</h2>";
+}else{
+
+    if(isset($_GET['id'])){
+        $_SESSION['cart'][$_GET['id']]=$_GET['qt'];
+    }
+    dd($_SESSION['cart']);
+
+}
+
+
+
+
+
