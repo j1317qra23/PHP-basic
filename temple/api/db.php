8da403781385d01c629aa8236b103d3103632869
Jason Tsai <qrass06040130@gmail.com>
2024-02-29 16:18:21 +0800

    0229

--- temple/api/db.php
+++ temple/api/db.php
@@ -3,7 +3,7 @@ date_default_timezone_set("Asia/Taipei");
 session_start();
 class DB{
 
-    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=db03";
+ protected $dsn = "mysql:host=localhost;charset=utf8;dbname=db03";
     protected $pdo;
     protected $table;
     
