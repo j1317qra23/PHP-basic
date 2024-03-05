3c9cf894b0cbce032d72d82a64dec36f47a63d85
Jason Tsai <qrass06040130@gmail.com>
2024-03-05 15:23:18 +0800

    0305

---
+++ 20240305/text-export.php
@@ -0,0 +1,90 @@
+<?php
+
+
+if(!empty($_POST)){
+$rows=all("20200706"," where  `投票所編號` in ('".join("','",$_POST['select'])."')");
+
+$filename=date("Ymd").rand(100000000,999999999);
+$file=fopen("./doc/{$filename}.csv",'w+');
+fwrite($file, "\xEF\xBB\xBF");
+$chk=false;
+foreach($rows as $row){
+    if(!$chk){
+        $cols=array_keys($row);
+        fwrite($file,join(",",$cols)."\r\n");
+        $chk=true;
+    }
+    fwrite($file,join(",",$row)."\r\n");
+}
+fclose($file);
+
+echo "<a href='./doc/{$filename}.csv'  download>檔案已匯出，請點此連結下載</a>";
+}
+
+
+?>
+
+<style>
+    table{
+        border-collapse: collapse;
+    }
+    td{
+        border:1px solid #666;
+        padding:5px 12px;
+    }
+    th{
+        border:1px solid #666;
+        padding:5px 12px;
+        background-color: black;
+        color:white;
+    }
+</style>
+<script src="./jquery-3.4.1.min.js"></script>
+<form action="?" method="post">
+    <input type="submit" value="匯出選擇的資料">
+<table>
+<tr>
+        <th>
+            <input type="checkbox" name="" id="select">
+            勾選</th>
+        <th>id</th>    
+        <th>訂單編號</th>
+        <th>金額</th>
+        <th>會員帳號</th>
+        <th>姓名</th>
+        <th>下單日期</th>
+        <th>信箱</th>
+        <th>地址</th>
+        <th>電話</th>
+        <th>cart</th>
+    </tr>
+<?php
+$rows=$Order->all('orders');
+foreach($rows as $row){
+    echo "<tr>";
+    echo "<td>";
+    echo "<input type='checkbox' name='select[]' value='{$row['投票所編號']}'>";
+    echo "</td>";
+    foreach($row as $value){
+        echo "<td>";
+        echo $value;
+        echo "</td>";
+    }
+    echo "</tr>";
+}
+
+
+?>
+</table>
+</form>
+
+
+<script>
+$("#select").on("change",function(){
+    if($(this).prop('checked')){
+        $("input[name='select[]']").prop('checked',true);
+    }else{
+        $("input[name='select[]']").prop('checked',false);
+    }
+})
+</script>
---
+++ 20240305/text-import.php
@@ -0,0 +1,87 @@
+<style>
+    table{
+        border-collapse: collapse;
+    }
+    td{
+        border:1px solid #666;
+        padding:5px 12px;
+    }
+    th{
+        border:1px solid #666;
+        padding:5px 12px;
+        background-color: black;
+        color:white;
+    }
+</style>
+<?php
+/****
+ * 1.建立資料庫及資料表
+ * 2.建立上傳檔案機制
+ * 3.取得檔案資源
+ * 4.取得檔案內容
+ * 5.建立SQL語法
+ * 6.寫入資料庫
+ * 7.結束檔案
+ */
+
+ if(!empty($_FILES['text']['tmp_name'])){
+    $filename=$_FILES['text']['name'];
+    $filesize=$_FILES['text']['size'];
+    echo "檔案上傳成功!檔名為:".$filename;
+    echo "<br>檔案大小:".$filesize;
+    echo "<br>";
+    $file=fopen($_FILES['text']['tmp_name'],'r');
+    $line=fgets($file);
+    
+    $cols=explode(",",$line);
+    echo "<table>";
+    echo "<tr>";
+    foreach($cols as $col){
+        echo "<th>";
+        echo $col;
+        echo "</th>";
+    }
+    echo "</tr>";
+    while(!feof($file)){
+        $line=fgets($file);
+        $cols=explode(",",$line);
+        echo "<tr>";
+        foreach($cols as $col){
+            echo "<td>";
+            echo $col;
+            echo "</td>";
+        }
+        echo "</tr>";
+
+    }
+    
+
+ }
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>文字檔案匯入</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1 class="header">文字檔案匯入練習</h1>
+<!---建立檔案上傳機制--->
+<form action="?" method="post" enctype="multipart/form-data">
+<input type="file" name="text" id="text">
+<input type="submit" value="上傳">
+
+</form>
+
+
+<!----讀出匯入完成的資料----->
+
+
+
+</body>
+</html>
